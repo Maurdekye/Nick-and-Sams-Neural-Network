2cb148a9a2271a39a9b8f6f9a39acc6a55c88589
Maurdekye <ncolaprete@gmail.com>
2017-05-25 20:32:29 -0400

    added version check to top of mnist.py

--- mnist.py
+++ mnist.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python
+import sys
+major_version, minor_version = sys.version_info[:2]
+if major_version < 3 or minor_version < 6:
+  raise Exception("Python version must be at least 3.6 to run")
+
 import numpy as np
 
 class MNISTFormatException(Exception):
