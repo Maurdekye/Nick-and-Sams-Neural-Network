61148363a35fb695dff217b86459877c771994e9
samuel <sam@samuels-MacBook-Pro-4.local>
2017-05-25 20:18:21 -0400

    translated format_for_network

---
+++ NeuralNetwork.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+def format_for_network(image: [([byte], byte)], label: byte) -> [[]]:
+    output = [[]]
+    output[label] = 1
+    input = [image.GetLength(0) * image.GetLength(1)]
+
+    index = 0
+
+    for x in image[0]:
+        for y in image[1]:
+            input[index] = image[x, y] / 255.0
+            index ++
+
+    return (input, output)
+
+
+
+
