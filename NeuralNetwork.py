84ca6c07f035d73c8af4862cf3d4f40a793a27c7
samuel <sam@samuels-MacBook-Pro-4.local>
2017-05-25 20:55:27 -0400

    updated

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -1,6 +1,8 @@
 import numpy as np
 
-def format_for_network(image: [([byte], byte)], label: byte) -> [[]]:
+
+
+def format_for_network(image: list, label: bytes) -> list:
     output = [[]]
     output[label] = 1
     input = [image.GetLength(0) * image.GetLength(1)]
@@ -10,10 +12,9 @@ def format_for_network(image: [([byte], byte)], label: byte) -> [[]]:
     for x in image[0]:
         for y in image[1]:
             input[index] = image[x, y] / 255.0
-            index ++
+            index += 1
 
     return (input, output)
-
 
 
 
